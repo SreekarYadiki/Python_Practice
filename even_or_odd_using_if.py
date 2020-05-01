5a3b082ffe512e3f00c052ecdc08b2c5469f34e0
SreekarYadiki <64364813+SreekarYadiki@users.noreply.github.com>
2020-05-01 11:05:36 +0530

    first program

---
+++ even_or_odd_using_if.py
@@ -0,0 +1,5 @@
+x = int(input("Please enter a number"))
+if x%2 == 0:
+    print("The number entered is ", x , "and it is an even number")
+else:
+    print("The number entered is", x , "and it is an odd number")
